19b0f57d97d52c33218f3d2303fa655eef9fc52e
Anders Ingemann <anders@ingemann.de>
2016-09-03 14:39:06 +0200

    More fixes

--- zsh/rc.linux.zsh
+++ zsh/rc.linux.zsh
@@ -1,8 +1,6 @@
 #!/bin/zsh
 alias ls='ls -h --color=auto'
 
-alias rm='rm -I'
-
 # enable color support of ls and also add handy aliases
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,7 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
 rcfiles=$HOME/.homesick/repos/rc-files
 
-system=`$rcfiles/system`
+system=$("$rcfiles/system")
 
 if [ -f ~/.custom ]; then
 	source ~/.custom
@@ -12,28 +12,28 @@ if [ -f ~/.localenv ]; then
 fi
 
 # homeshick stuff
-source $HOME/.homesick/repos/homeshick/homeshick.sh
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 # Check if castles need refreshing
 homeshick --quiet refresh 14 $HOMESHICK_REFRESH_REPOS
 unset HOMESHICK_REFRESH_REPOS
 fpath=($HOME/.homesick/repos/homeshick/completions $fpath)
 
 omz_dir=$HOME/.homesick/repos/oh-my-zsh
-if [[ -e $omz_dir/oh-my-zsh.sh ]] then
+if [[ -e $omz_dir/oh-my-zsh.sh ]]; then
 	export ZSH=$omz_dir
 	# Let homeshick handle the updating
 	DISABLE_AUTO_UPDATE="true"
-	if [[ -z "$ZSH_THEME" ]] then
+	if [[ -z "$ZSH_THEME" ]]; then
 		ZSH_THEME="jreese"
 	fi
-	plugins+=(ant cake coffee docker extract history-substring-search pip colored-man vagrant)
+	plugins+=()
 	if [[ $system == 'Linux' ]]; then
 		plugins+=()
 	fi
 	if [[ $system == 'OSX' ]]; then
-		plugins+=(brew terminalapp osx)
+		plugins+=(osx)
 	fi
-	source $omz_dir/oh-my-zsh.sh
+	source "$omz_dir/oh-my-zsh.sh"
 	unsetopt correct_all
 	# Disable the automatic titling, it screws up tmux
 	DISABLE_AUTO_TITLE=true
@@ -41,34 +41,22 @@ fi
 unset omz_dir
 
 if [[ $system == 'Linux' ]]; then
-	source $rcfiles/zsh/rc.linux.zsh
+	source "$rcfiles/zsh/rc.linux.zsh"
 fi
 if [[ $system == 'OSX' ]]; then
-	source $rcfiles/zsh/rc.osx.zsh
+	source "$rcfiles/zsh/rc.osx.zsh"
 fi
 
-source $rcfiles/aliases
-source $rcfiles/tools
+source "$rcfiles/aliases"
+source "$rcfiles/tools"
 
 if [[ -f ~/.dir_colors && ( -x /usr/local/bin/dircolors || -x /usr/bin/dircolors ) ]]; then
-    eval `dircolors ~/.dir_colors`
+    eval $(dircolors ~/.dir_colors)
 fi
 
-if [[ -e ~/.ssh/ssh_auth_sock ]] then
+if [[ -e ~/.ssh/ssh_auth_sock ]]; then
 	export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
 fi
-
-compile-zshrc () {
-	homesick_repos=$HOME/.homesick/repos
-	if [[ -n $1 && $1 == "clean" ]]; then
-		find $homesick_repos -name '*.zwc' -delete
-		echo 'All *.zwc files removed'
-		return
-	fi;
-	for file in `find $homesick_repos -name '*.zsh' -type f -print`; do
-		zcompile $file
-	done
-}
 
 unset rcfiles
 unset system
