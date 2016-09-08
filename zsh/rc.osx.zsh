3398982084843b423a189c9116141f26568af429
Anders Ingemann <aim@secoya.dk>
2016-09-08 10:37:29 +0200

    Remove ls aliasing, oh-my-zsh does that for us

--- zsh/rc.osx.zsh
+++ zsh/rc.osx.zsh
@@ -4,12 +4,6 @@ export LSCOLORS="gxfxcxdxbxegedabagacad"
 
 bindkey "^[[3~" delete-char
 
-if [[ -x /usr/local/bin/gls ]]; then
-    alias ls='/usr/local/bin/gls -h --color=auto'
-else
-    alias ls='ls -hG'
-fi
-
 if [[ -z "$LANG" ]]; then
 	export LANG='en_US.UTF-8'
 fi
