a719cf7e9b15fc896675dade14556b8333f79162
Anders Ingemann <anders@ingemann.de>
2016-11-23 18:53:19 +0100

    No need for autocolor and LSCOLOR oh-my-zsh does that

--- zsh/rc.linux.zsh
+++ zsh/rc.linux.zsh
@@ -1,7 +1,1 @@
 #!/bin/zsh
-alias ls='ls -h --color=auto'
-
-# enable color support of ls and also add handy aliases
-alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
--- zsh/rc.osx.zsh
+++ zsh/rc.osx.zsh
@@ -1,8 +1,5 @@
 #!/bin/zsh
 export CLICOLOR=1
-export LSCOLORS="gxfxcxdxbxegedabagacad"
-
-bindkey "^[[3~" delete-char
 
 if [[ -z "$LANG" ]]; then
 	export LANG='en_US.UTF-8'
